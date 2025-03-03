3f82e211fd7ebe0bcaa8f3117cda1476fceb22cc
hayam-tarek <hayamtareq28@gmail.com>
2025-03-04 00:13:00 +0200

    splash ui done

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+  static const Color primaryColor = Color(0xff100b20);
+}
---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,25 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: double.infinity,
+          ),
+          Image.asset(
+            AppAssets.logo,
+          ),
+        ],
+      ),
+    );
+  }
+}
