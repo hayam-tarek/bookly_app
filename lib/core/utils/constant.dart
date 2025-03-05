b0fb28caabb2abf7c63336e9d7e298ff0b5d5041
hayam-tarek <hayamtareq28@gmail.com>
2025-03-05 23:48:03 +0200

    refactor code and create home feature

---
+++ lib/core/utils/constant.dart
@@ -0,0 +1,1 @@
+const viewtransitionDuration = Duration(milliseconds: 500);
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,5 +1,8 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:bookly_app/core/utils/constant.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -7,34 +10,43 @@ class SplashViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(24.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        spacing: 16,
-        children: [
-          SizedBox(
-            width: double.infinity,
+    navigateToHomeView();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      spacing: 16,
+      children: [
+        SizedBox(
+          width: double.infinity,
+        ),
+        DefaultTextStyle(
+          style: GoogleFonts.limelight(
+            fontSize: MediaQuery.of(context).size.width * 0.20,
           ),
-          DefaultTextStyle(
-            style: GoogleFonts.limelight(
-              fontSize: MediaQuery.of(context).size.width * 0.20,
-            ),
-            child: AnimatedTextKit(
-              animatedTexts: [
-                WavyAnimatedText(
-                  'Bookly',
-                ),
-              ],
-              isRepeatingAnimation: false,
-            ),
+          child: AnimatedTextKit(
+            animatedTexts: [
+              WavyAnimatedText(
+                'Bookly',
+                speed: const Duration(milliseconds: 300),
+              ),
+            ],
+            isRepeatingAnimation: false,
           ),
-          Text(
-            "Read Freely, Anytime, Anywhere!",
-          ),
-        ],
-      ),
+        ),
+        Text(
+          "Read Freely, Anytime, Anywhere!",
+        ),
+      ],
     );
+  }
+
+  void navigateToHomeView() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(seconds: 3), () {
+        Get.to(HomeView(),
+            transition: Transition.rightToLeft,
+            duration: viewtransitionDuration);
+      });
+    });
   }
 }
