764f9c4e47bfde485cc20802108e049fb35cfeae
hayam-tarek <hayamtareq28@gmail.com>
2025-03-08 23:51:53 +0200

    create home repo

---
+++ lib/core/errors/failure.dart
@@ -0,0 +1,11 @@
+class Failure {}
+
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
+
+class NetworkFailure extends Failure {}
+
+class LocalFailure extends Failure {}
+
+class UnknownFailure extends Failure {}
