c3b30d8ae08a664b6a9044157921f05f8c7d4b5e
hayam-tarek <hayamtareq28@gmail.com>
2025-03-10 00:09:02 +0200

    created newest books cubit

---
+++ lib/features/home/presentation/view_model/newest_books/newest_books_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+  List<BookModel> books = [];
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.getFeaturedBooks();
+    result.fold(
+      (failure) => emit(NewestBooksFailure(failure.message)),
+      (books) => emit(NewestBooksSuccess(books)),
+    );
+  }
+}
